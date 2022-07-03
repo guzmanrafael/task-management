95d834f7197824c31a29e30dc397bc2c8018fb6c
Rafael Guzman <rafael.guzman1609@gmail.com>
2022-07-03 13:31:13 -0700

    Add dotenv module to load environment variables

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,11 @@ import { Task } from '../models/Task';
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.HOST,
   port: 5432,
-  username: 'postgres',
-  password: 'password',
-  database: 'todo',
+  username: process.env.DBUSERNAME,
+  password: process.env.DBPASSWORD,
+  database: process.env.DATABASE,
   synchronize: true,
   logging: false,
   entities: [Task],
