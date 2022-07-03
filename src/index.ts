d91f23e15542df9fc17b19e85e049c651cc4e787
Rafael Guzman <rafael.guzman1609@gmail.com>
2022-07-03 13:11:34 -0700

    Add customizable error handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import database from "./config/database";
+import handleError from "./middlewares/handleError";
 
 const app = express();
 
@@ -11,6 +12,8 @@ database
   .initialize()
   .then(() => console.log("Database connected"))
   .catch(console.error);
+
+app.use(handleError);
 
 app.listen(3030, () => {
   console.log("App excute in port 3030");
