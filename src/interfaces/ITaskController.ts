fabab2acb48d917199a8433eae2467095b2e6a8d
Rafael Guzman <rafael.guzman1609@gmail.com>
2022-07-04 12:24:26 -0700

    Add update method in controller layer

--- src/interfaces/ITaskController.ts
+++ src/interfaces/ITaskController.ts
@@ -4,5 +4,6 @@ export interface ITaskController {
     create(req: Request, res: Response, next: NextFunction): Promise<any>;
     getAll(req: Request, res: Response, next: NextFunction): Promise<any>;
     getById(req: Request, res: Response, next: NextFunction): Promise<any>;
+    update(req: Request, res: Response, next: NextFunction): Promise<any>;
   }
   
