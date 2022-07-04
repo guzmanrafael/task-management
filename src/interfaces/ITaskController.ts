9fda8d2005f07e41de6cfbad85269fbaf86576a2
Rafael Guzman <rafael.guzman1609@gmail.com>
2022-07-04 12:01:46 -0700

    Add getbyId method in controller layer

--- src/interfaces/ITaskController.ts
+++ src/interfaces/ITaskController.ts
@@ -3,5 +3,6 @@ import { Request, Response, NextFunction } from "express";
 export interface ITaskController {
     create(req: Request, res: Response, next: NextFunction): Promise<any>;
     getAll(req: Request, res: Response, next: NextFunction): Promise<any>;
+    getById(req: Request, res: Response, next: NextFunction): Promise<any>;
   }
   
