46dde2a90002be42e5bfe3bd9df82e126974f730
Rafael Guzman <rafael.guzman1609@gmail.com>
2022-07-04 14:47:09 -0700

    refactor custom errors

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -1,16 +1,13 @@
 import jwt from "jsonwebtoken";
+import { INVALID_TOKEN, TOKEN_MUST_BE_SENT } from "../commons/errors";
 import { CustomError } from "../models/CustomError";
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
-  if (!token) {
-    throw new CustomError('Token must be sent', 400, '');
-  }
+  if (!token) TOKEN_MUST_BE_SENT();
 
   jwt.verify(token, process.env.TOKEN_SECRET, function (err, decodedToken) {
-    if (err) {
-      throw new CustomError('Invalid token', 401, '');
-    }
+    if (err) INVALID_TOKEN();
 
     req.user = decodedToken.user;
     next();
